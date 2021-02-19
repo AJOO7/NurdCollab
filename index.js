0c0ee5663488ce0e55e667d2e3e349385baed4dc
AJ00007 <67185443+AJ00007@users.noreply.github.com>
2021-02-19 17:13:36 +0530

    adding path and view

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
+const editorSockets = require('./config/editor_sockets').editorSockets(server);
+
+app.use(express.static('assets'))
+
+const path = require('path');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/', require('./routes'));
 
 server.listen(process.env.PORT || 3000);
 console.log("working fine");
