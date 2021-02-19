c44221903b84bc66cc4a76ca39de37c66f85f332
AJ00007 <67185443+AJ00007@users.noreply.github.com>
2021-02-19 16:33:11 +0530

    adding express layouts

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
+// extract style and scripts from sub pages into the layout
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 server.listen(process.env.PORT || 3000);
+console.log("working fine");
