2aadfbc6f4314f10b5e1e7b3e132c80cb0e3ab78
AJ00007 <67185443+AJ00007@users.noreply.github.com>
2021-02-20 23:05:02 +0530

    tagging

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-
+// initialising peerjs server
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
     debug: true,
@@ -13,14 +13,16 @@ const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
+// sockets config
 const editorSockets = require('./config/editor_sockets').editorSockets(server);
-
+// css js images
 app.use(express.static('assets'))
 
 const path = require('path');
+// following MVC pattern
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/', require('./routes'));
 
 server.listen(process.env.PORT || 3000);
-console.log("working fine");
+// console.log("working fine");
