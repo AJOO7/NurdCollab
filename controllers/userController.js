5ac14dc595134e179efd639a6575d46d608d9995
AJ00007 <67185443+AJ00007@users.noreply.github.com>
2021-02-20 22:37:27 +0530

    tagging

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,13 @@
 const path = require('path');
+// uuid for creating a unique id 
 const { v4: uuidV4 } = require('uuid');
-
+// create a new uuid and rediect to userEditor through router->controller
 module.exports.userEditorCreate = function (req, res) {
 
     return res.redirect(`/users/${uuidV4()}`);
 
 }
+// joining a created unique room
 module.exports.userEditor = function (req, res) {
 
     return res.render('user-editor', {
