cc594c9bf22c8f48049441b1636478896601d05a
AJ00007 <67185443+AJ00007@users.noreply.github.com>
2021-02-20 22:33:45 +0530

    tagging

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
+// Following MVC pattern
+// usercontroller control renderig for editor page
 const userController = require('../controllers/userController');
 const express = require('express');
 const router = express.Router();
+// using userEditorCreate for providing a new uuid for the editor page
 router.get('/', userController.userEditorCreate);
+// using userEditor for joining a previously created room id
 router.get('/:room', userController.userEditor);
 module.exports = router;
