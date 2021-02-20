fb117f041dd6104f53ac0df8188405eef02d5962
AJ00007 <67185443+AJ00007@users.noreply.github.com>
2021-02-20 22:30:53 +0530

    tagging

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
+// Following MVC pattern
+// homecontroller control rendering for home page
 const homecontroller = require('../controllers/homeController');
 const express = require('express');
 const router = express.Router();
+// calling home component of home controllers for home page
 router.get('/', homecontroller.home);
+// redirecting to user router for user creating and joinig
 router.use('/users', require('./users'));
 module.exports = router;
